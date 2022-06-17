2d832d4a129efaecf7d362014f99c9f1485e0726
diyoraniraj <diyoraniraj111@gmail.com>
2022-06-17 17:45:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter , Routes, Route  } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./Pages/Navbar";
+import Protected from "./Pages/Protected";
+import Home  from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setisLoggedIn] = useState(null);
+  const logIn = () => {
+    setisLoggedIn(true);
+  };
+  const logOut = () => {
+    setisLoggedIn(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        {isLoggedIn ? (
+          <button onClick={logOut}>Logout</button>
+        ) : (
+          <button onClick={logIn}>Login</button>
+        )}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Protected isLoggedIn={isLoggedIn}><Profile /></Protected>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      </BrowserRouter> 
   );
 }
-
 export default App;
